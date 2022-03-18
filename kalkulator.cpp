3210fb26acff1b1e82cc6787e693f3146234a1c8
Umenkk <mirfanaryaa@gmail.com>
2022-03-18 16:28:56 +0700

    a

---
+++ kalkulator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+
+void tambah(float a, float b){
+	cout<<"Hasil dari Penambahan " <<a<<" + "<<b<<" adalah = ";
+    cout<<a+b;
+}
+void kurang(float a, float b){
+	cout<<"Hasil dari Pengurangan "<<a<<" - "<<b<<" adalah = ";
+	cout<<a-b;
