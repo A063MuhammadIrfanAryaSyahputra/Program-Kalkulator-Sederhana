78c16c8ece1377323b1eb092d38f71239c9c7d47
Umenkk <muhammad2100018063@webmail.uad.ac.id>
2022-03-18 20:04:14 +0700

    change

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -20,47 +20,4 @@ void bagi(float a, float b){
 	cout<<a/b; 
 }
 
-	int main(){
-	int pilih;
-	float a,b;
-	char k, ulang;
-	do{
-	cout<<"||==========================================||\n";
-    cout<<"||===========||POST TEST DDP 8||============||\n";
-	cout<<"||=========||KALKULATOR SEDERHANA||=========||\n";
-	cout<<"||==========================================||\n";
-	cout<<"||1. Penambahan                             ||\n";
-	cout<<"||2. Pengurangan                            ||\n";
-	cout<<"||3. Perkalian                              ||\n";
-	cout<<"||4. Pembagian                              ||\n";
-	cout<<"||==========================================||\n";
-	cin>>pilih;
-    cout<<"||==========================================||\n";
-    
-    if (pilih==1){
-    	cout<<"Masukkan bilangan pertama ="; cin>>a;
-    	cout<<"Masukkan bilangan kedua   ="; cin>>b;
-    	tambah(a,b);
-    	cout<<"\nUlang? Y/T :"; cin>>ulang;
-	}
-	else if (pilih==2){
-		cout<<"Masukkan bilangan pertama ="; cin>>a;
-    	cout<<"Masukkan bilangan kedua   ="; cin>>b;
-    	kurang(a,b);
-    	cout<<"\nUlang? Y/T :"; cin>>ulang;
-	}
-	else if(pilih==3){
-		cout<<"Masukkan bilangan pertama ="; cin>>a;
-    	cout<<"Masukkan bilangan kedua   ="; cin>>b;
-    	kali(a,b);
-    	cout<<"\nUlang? Y/T :"; cin>>ulang;
-	}
-	else if(pilih==4){
-		cout<<"Masukkan bilangan pertama ="; cin>>a;
-    	cout<<"Masukkan bilangan kedua   ="; cin>>b;
-    	bagi(a,b);
-    	cout<<"\nUlang? Y/T :"; cin>>ulang;
-	}
-	}while (ulang=='Y'||ulang !='T');
-	
-}
+
