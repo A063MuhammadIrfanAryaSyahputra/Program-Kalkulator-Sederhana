c13a2d1e18086f7d669943c13740b0674ade9181
Umenkk <muhammad2100018063@webmail.uad.ac.id>
2022-03-18 20:03:28 +0700

    Merge branch 'main' of https://github.com/A063MuhammadIrfanAryaSyahputra/Program-Kalkulator-Sederhana

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<conio.h>
 #include<stdlib.h>
 using namespace std;
 
@@ -9,3 +10,57 @@ void tambah(float a, float b){
 void kurang(float a, float b){
 	cout<<"Hasil dari Pengurangan "<<a<<" - "<<b<<" adalah = ";
 	cout<<a-b;
+}
+void kali(float a, float b){
+	cout<<"Hasil dari Perkalian "<<a<<" x "<<b<<" adalah = ";
+	cout<<a*b;
+}
+void bagi(float a, float b){
+	cout<<"Hasil dari Pembagian "<<a<<" : "<<b<<" adalah = ";
+	cout<<a/b; 
+}
+
+	int main(){
+	int pilih;
+	float a,b;
+	char k, ulang;
+	do{
+	cout<<"||==========================================||\n";
+    cout<<"||===========||POST TEST DDP 8||============||\n";
+	cout<<"||=========||KALKULATOR SEDERHANA||=========||\n";
+	cout<<"||==========================================||\n";
+	cout<<"||1. Penambahan                             ||\n";
+	cout<<"||2. Pengurangan                            ||\n";
+	cout<<"||3. Perkalian                              ||\n";
+	cout<<"||4. Pembagian                              ||\n";
+	cout<<"||==========================================||\n";
+	cin>>pilih;
+    cout<<"||==========================================||\n";
+    
+    if (pilih==1){
+    	cout<<"Masukkan bilangan pertama ="; cin>>a;
+    	cout<<"Masukkan bilangan kedua   ="; cin>>b;
+    	tambah(a,b);
+    	cout<<"\nUlang? Y/T :"; cin>>ulang;
+	}
+	else if (pilih==2){
+		cout<<"Masukkan bilangan pertama ="; cin>>a;
+    	cout<<"Masukkan bilangan kedua   ="; cin>>b;
+    	kurang(a,b);
+    	cout<<"\nUlang? Y/T :"; cin>>ulang;
+	}
+	else if(pilih==3){
+		cout<<"Masukkan bilangan pertama ="; cin>>a;
+    	cout<<"Masukkan bilangan kedua   ="; cin>>b;
+    	kali(a,b);
+    	cout<<"\nUlang? Y/T :"; cin>>ulang;
+	}
+	else if(pilih==4){
+		cout<<"Masukkan bilangan pertama ="; cin>>a;
+    	cout<<"Masukkan bilangan kedua   ="; cin>>b;
+    	bagi(a,b);
+    	cout<<"\nUlang? Y/T :"; cin>>ulang;
+	}
+	}while (ulang=='Y'||ulang !='T');
+	
+}
